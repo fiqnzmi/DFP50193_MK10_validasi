2e88155e2ab9e68cbea79f4c4b43f4951c8f4a01
fiqnzmi <muhammadafiqnazmi2003@gmail.com>
2026-03-18 05:17:10 +0800

    SIAP

--- index.php
+++ index.php
@@ -1,75 +1,82 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Borang Pinjaman Komputer</title>
-<link rel="stylesheet" href="style.css">
+    <title>Borang Pinjaman Komputer</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-<div class="container">
+    <div class="container">
 
-<div class="logo-container">
-<img src="kpt.png" class="logo">
-<img src="poly.png" class="logo">
-</div>
+        <div class="logo-container">
+            <img src="kpt.png" class="logo">
+            <img src="poly.png" class="logo">
+        </div>
 
-<h2 class="title">Borang Permohonan Skim Pinjaman Komputer Riba</h2>
-<form class="form-box" action="process.php" method="POST">
+        <h2 class="title">Borang Permohonan Skim Pinjaman Komputer Riba</h2>
+        <form class="form-box" action="process.php" method="POST">
 
-<label class="label">Nama Pelajar</label>
-<input type="text" name="nama" class="input">
+            <label class="label">Nama Pelajar</label>
+            <input type="text" name="nama" class="input">
 
-<label class="label">No Matriks</label>
-<input type="text" name="matrik" class="input">
+            <label class="label">No Matriks</label>
+            <input type="text" name="matrik" class="input">
 
-<label class="label">Umur</label>
-<input type="number" name="umur" class="input">
+            <label class="label">Umur</label>
+            <input type="number" name="umur" class="input">
 
-<label class="label">Program Pengajian</label>
-<select name="program" class="input">
-<option value="">-- Pilih Program --</option>
-<option value="Diploma Teknologi Kreatif Digital Produksi Video">Diploma Teknologi Kreatif Digital Produksi Video</option>
-<option value="Diploma Teknologi Kreatif Digital Seni">Diploma Teknologi Kreatif Digital Seni</option>
-<option value="Diploma Perakaunan">Diploma Perakaunan</option>
-<option value="Diploma Sains Kesetiausahaan">Diploma Sains Kesetiausahaan</option>
-<option value="Diploma Pengajian Perniagaan">Diploma Pengajian Perniagaan</option>
-<option value="Diploma Kejuruteraan Mekanikal">Diploma Kejuruteraan Mekanikal</option>
-<option value="Diploma Perkhidmatan Makanan Amalan Halal">Diploma Perkhidmatan Makanan Amalan Halal</option>
-<option value="Diploma Pengurusan Resort">Diploma Pengurusan Resort</option>
-<option value="Diploma Pengurusan Pelancongan">Diploma Pengurusan Pelancongan</option>
-<option value="Diploma Pengurusan Acara">Diploma Pengurusan Acara</option>
-<option value="Diploma Teknologi Maklumat">Diploma Teknologi Maklumat</option>
-<option value="Diploma Kejuruteraan Elektronik (Telekomunikasi)">Diploma Kejuruteraan Elektronik (Telekomunikasi)</option>
-<option value="Diploma Kejuruteraan Elektronik">Diploma Kejuruteraan Elektronik</option>
-<option value="Diploma Kejuruteraan Elektronik (Komputer)">Diploma Kejuruteraan Elektronik (Komputer)</option>
-</select>
+            <label class="label">Program Pengajian</label>
+            <select name="program" class="input">
+                <option value="">-- Pilih Program --</option>
+                <option value="Diploma Teknologi Kreatif Digital Produksi Video">Diploma Teknologi Kreatif Digital
+                    Produksi Video</option>
+                <option value="Diploma Teknologi Kreatif Digital Seni">Diploma Teknologi Kreatif Digital Seni</option>
+                <option value="Diploma Perakaunan">Diploma Perakaunan</option>
+                <option value="Diploma Sains Kesetiausahaan">Diploma Sains Kesetiausahaan</option>
+                <option value="Diploma Pengajian Perniagaan">Diploma Pengajian Perniagaan</option>
+                <option value="Diploma Kejuruteraan Mekanikal">Diploma Kejuruteraan Mekanikal</option>
+                <option value="Diploma Perkhidmatan Makanan Amalan Halal">Diploma Perkhidmatan Makanan Amalan Halal
+                </option>
+                <option value="Diploma Pengurusan Resort">Diploma Pengurusan Resort</option>
+                <option value="Diploma Pengurusan Pelancongan">Diploma Pengurusan Pelancongan</option>
+                <option value="Diploma Pengurusan Acara">Diploma Pengurusan Acara</option>
+                <option value="Diploma Teknologi Maklumat">Diploma Teknologi Maklumat</option>
+                <option value="Diploma Kejuruteraan Elektronik (Telekomunikasi)">Diploma Kejuruteraan Elektronik
+                    (Telekomunikasi)</option>
+                <option value="Diploma Kejuruteraan Elektronik">Diploma Kejuruteraan Elektronik</option>
+                <option value="Diploma Kejuruteraan Elektronik (Komputer)">Diploma Kejuruteraan Elektronik (Komputer)
+                </option>
+            </select>
 
-<label class="label">Kemudahan Internet</label>
+            <label class="label">Jenis Laptop Diperlukan</label>
 
-<input type="checkbox" name="internet[]" value="WiFi" class="check"> WiFi
-<input type="checkbox" name="internet[]" value="Data Mobile" class="check"> Data Mobile
+            <input type="radio" name="jenis_laptop" value="Standard (Windows)" class="radio"> Standard (Windows)
+            <input type="radio" name="jenis_laptop" value="Macbook" class="radio"> Macbook
 
-<br><br>
+            <br><br>
 
-<label class="label">Status Pelajar</label>
+            <label class="label">Tujuan Penggunaan</label>
 
-<input type="radio" name="status" value="Sepenuh Masa" class="radio"> Sepenuh Masa
-<input type="radio" name="status" value="Separuh Masa" class="radio"> Separuh Masa
+            <input type="checkbox" name="tujuan[]" value="Assignment" class="check"> Assignment
+            <input type="checkbox" name="tujuan[]" value="Programming" class="check"> Programming
+            <input type="checkbox" name="tujuan[]" value="Design" class="check"> Design
 
-<br><br>
+            <br><br>
 
-<label class="label">Alasan Permohonan</label>
-<textarea name="alasan" class="textarea"></textarea>
+            <label class="label">Alasan Permohonan</label>
+            <textarea name="alasan" class="textarea"></textarea>
 
-<br><br>
+            <br><br>
 
-<button type="submit" class="btn-submit">Hantar</button>
-<button type="reset" class="btn-reset">Tetap Semula</button>
+            <button type="submit" class="btn-submit">Hantar</button>
+            <button type="reset" class="btn-reset">Tetap Semula</button>
 
-</form>
+        </form>
 
-</div>
+    </div>
 
 </body>
+
 </html>
--- process.php
+++ process.php
@@ -1,53 +1,50 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Keputusan Permohonan</title>
-<link rel="stylesheet" href="style.css">
+    <title>Keputusan Permohonan</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-<div class="container">
+    <div class="container">
 
-<h2 class="title">Keputusan Permohonan</h2>
+        <h2 class="title">Keputusan Permohonan</h2>
 
-<?php
+        <?php
 
-$nama = $_POST['nama'];
-$matrik = $_POST['matrik'];
-$umur = $_POST['umur'];
-$program = $_POST['program'];
-$status = $_POST['status'];
-$alasan = $_POST['alasan'];
+        $nama = $_POST['nama'];
+        $matrik = $_POST['matrik'];
+        $umur = $_POST['umur'];
+        $program = $_POST['program'];
+        $jenis_laptop = $_POST['jenis_laptop'];
+        $tujuan = $_POST['tujuan'] ?? [];
+        $alasan = $_POST['alasan'];
 
-if(empty($nama) || empty($matrik) || empty($umur) || empty($program) || empty($status) || empty($alasan))
-{
-    echo "<p class='error'>Ralat: Semua maklumat mesti diisi.</p>";
-}
+        if (empty($nama) || empty($matrik) || empty($umur) || empty($program) || empty($jenis_laptop) || empty($tujuan) || empty($alasan)) {
+            echo "<p class='error'>Ralat: Semua maklumat mesti diisi.</p>";
+        } else if (strlen($alasan) < 25) {
+            echo "<p class='error'>Ralat: Alasan mesti sekurang-kurangnya 25 aksara.</p>";
+        } else {
+            echo "<p class='success'>Permohonan berjaya dihantar.</p>";
 
-else if(strlen($alasan) < 25)
-{
-    echo "<p class='error'>Ralat: Alasan mesti sekurang-kurangnya 25 aksara.</p>";
-}
+            echo "<p>Nama: $nama</p>";
+            echo "<p>No Matriks: $matrik</p>";
+            echo "<p>Umur: $umur</p>";
+            echo "<p>Program: $program</p>";
+            echo "<p>Jenis Laptop Diperlukan: $jenis_laptop</p>";
+            echo "<p>Tujuan Penggunaan: " . implode(', ', $tujuan) . "</p>";
+            echo "<p>Alasan: $alasan</p>";
+        }
 
-else
-{
-    echo "<p class='success'>Permohonan berjaya dihantar.</p>";
+        ?>
 
-    echo "<p>Nama: $nama</p>";
-    echo "<p>No Matriks: $matrik</p>";
-    echo "<p>Umur: $umur</p>";
-    echo "<p>Program: $program</p>";
-    echo "<p>Status: $status</p>";
-    echo "<p>Alasan: $alasan</p>";
-}
+        <br>
+        <a href="index.php" class="link">Kembali ke Borang</a>
 
-?>
-
-<br>
-<a href="index.php" class="link">Kembali ke Borang</a>
-
-</div>
+    </div>
 
 </body>
+
 </html>
